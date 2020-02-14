81b5bd03f7e8f989ab3ffd0c1f502769da6f8167
yoinkbird <YoinkBird2010@gmail.com>
2020-02-14 14:44:19 -0600

    test - updating test runner

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ set -x
 set -eu
 arg="${1:-}"
 if [[ -z "${arg}" ]]; then
-  docker-compose up --abort-on-container-exit
+  # too cumbersome: # docker-compose up --abort-on-container-exit
+  echo "compile java and run bash e2e test"
+  docker-compose up --build
   rc=$?
 elif [[ "${arg}" == "test" ]]; then
   docker-compose run --rm --no-deps --service-ports --entrypoint bash test
