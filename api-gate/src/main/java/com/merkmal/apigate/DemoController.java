1f9924513039994f83c987b52462cd46e32c4917
yoinkbird <YoinkBird2010@gmail.com>
2020-02-14 10:00:56 -0600

    BROKEN - error commit to track progress

--- api-gate/src/main/java/com/merkmal/apigate/DemoController.java
+++ api-gate/src/main/java/com/merkmal/apigate/DemoController.java
@@ -1,20 +1,62 @@
 package com.merkmal.apigate;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+class DemoController {
 
-	private static final String template = "Hello, %s!";
-	//private final AtomicLong counter = new AtomicLong();
+  private final DemoRepository repository;
 
-	@GetMapping("/demo")
-	public Demo demo(@RequestParam(value = "name", defaultValue = "World") String name) {
-		//return new Demo(counter.incrementAndGet(), String.format(template, name));
-		return new Demo(String.format(template, name));
-	}
+  DemoController(DemoRepository repository) {
+    this.repository = repository;
+  }
+
+  // Aggregate root
+
+  @GetMapping("/demos")
+  List<Demo> all() {
+    return repository.findAll();
+  }
+
+  @PostMapping("/demos")
+  Demo newDemo(@RequestBody Demo newDemo) {
+    return repository.save(newDemo);
+  }
+
+  // Single item
+
+  @GetMapping("/demos/{id}")
+  Demo one(@PathVariable Long id) {
+
+    return repository.findById(id);
+//      .orElseThrow(() -> new DemoNotFoundException(id));
+  }
+
+  @PutMapping("/demos/{id}")
+  Demo replaceDemo(@RequestBody Demo newDemo, @PathVariable Long id) {
+
+    return repository.findById(id)
+      .map(demo -> {
+        demo.setContent(newDemo.getContent());
+        //demo.setRole(newDemo.getRole());
+        return repository.save(demo);
+      })
+      .orElseGet(() -> {
+        newDemo.setId(id);
+        return repository.save(newDemo);
+      });
+  }
+
+  @DeleteMapping("/demos/{id}")
+  void deleteDemo(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
--- worklog.md
+++ worklog.md
@@ -6,3 +6,13 @@ api-gate/
 morph to follow: https://spring.io/guides/tutorials/bookmarks/
 but without the `LoadDatabase`
 but not working, so use simpler implementation from: https://spring.io/guides/gs/rest-service/#_create_a_resource_controller
+
+implementing https://spring.io/guides/tutorials/bookmarks/#_http_is_the_platform
+Broken:
+```
+$ ./gradlew bootRun
+
+> Task :compileJava FAILED
+/home/yoinkbird/workspace/search3/knime/unbemerkenswert/api-gate/src/main/java/com/merkmal/apigate/DemoController.java:39: error: incompatible types: Optional<Demo> cannot be converted to Demo
+    return repository.findById(id);
+```
